b8331bf13268e070592770796f4b2ac13fe9d41a
Sandeep Kumar Singh <sandeep908singh908@gmail.com>
2023-08-31 11:45:25 +0530

    Create Pattern4.java

---
+++ Pattern4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Pattern4 {
+
+  // Pattern-4
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in) ;
+    int n = sc.nextInt() ;
+    for(int i=1 ;i<=n ;i++){
+      for(int j=1 ;j<=i-1 ;j++){
+        System.out.print(" ");
+      }
+      for(int j=1 ;j<=n+1-i ;j++){
+        System.out.print("*");
+      }
+      
+      System.out.println();
+    }
+  }
+}
